1d31ba2567ffb05bd82029d3cd81c09dee45ddb4
MindeRe <MindeRe@DESKTOP-CND4IL3>
2024-05-20 22:21:53 +0300

    laba9 update

--- лаба 9/script.js
+++ лаба 9/script.js
@@ -27,5 +27,12 @@ function helN()
 	var h=window.location.href;
 	nf=nf.substring(0,1);
 	otch=otch.substring(0,1);
-	document.write("Добрый день, "+name.toUpperCase()+" "+nf.toUpperCase()+". "+otch.toUpperCase()+". <br> "+h+"<br>");
+	document.write("Добрый день, "+name.toUpperCase()+" "+nf.toUpperCase()+". "+otch.toUpperCase()+". <br> "+" Мы рады приветствовать вас на сайте "+h+"<br>");
+}
+function calculation()
+{
+	var v1=parseInt(document.form1.in1.value);
+	var v2=parseInt(document.form1.in2.value);
+	var v3=v1+v2;
+	document.form1.res.value=v3;
 }
