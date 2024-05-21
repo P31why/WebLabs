608cf44cc6a8ddf9e812269df121baf053fefcd1
MindeRe <MindeRe@DESKTOP-CND4IL3>
2024-05-21 20:54:41 +0300

    pr 13 & other

--- пр 12/script.js
+++ пр 12/script.js
@@ -8,8 +8,7 @@ function t1(){
 		document.form1.y.value=y1;
 	}
 }
-function t2()
-{
+function t2(){
 	var a=document.form2.a.value;
 	var b=document.form2.b.value;
 	var c=document.form2.c.value;
@@ -22,5 +21,20 @@ function t2()
 	else if(c>a && c>b && a>b){
 		document.form2.out.value="Заправить колонку B";
 	}
+	
+}
+function t3(){
+	var a=parseInt(document.form3.a1.value);
+	var n1=parseInt(a/100);
+	var n2=parseInt(a/10%10);
+	var n3=parseInt(a%10);
+	if(n1===5 || n2===5 || n3===5){
+		document.form3.out.value="Есть";
+	}
+	else {
+		document.form3.out.value="Нет";
+	}
+}
+function t4(){
 	
 }
