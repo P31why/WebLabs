4d13ec9730e46a8abb7301c314ed2ba782fadf59
MindeRe <MindeRe@DESKTOP-CND4IL3>
2024-05-21 21:46:09 +0300

    pr13 update

---
+++ пр 13/script.js
@@ -0,0 +1,9 @@
+var arr=['a','b','c'];
+alert(arr[0]+", "+arr[1]+", "+arr[2]);
+document.write(arr[0]+", "+arr[1]+", "+arr[2]+'<br>');
+arr=['a','b','c','d'];
+var str=arr[0]+'+'+arr[1]+', '+arr[2]+'+'+arr[3];
+document.write(str);
+arr=[2,5,3,9];
+var result=(arr[0]*arr[1])+(arr[2]+arr[3]);
+document.write('<br>'+result);
