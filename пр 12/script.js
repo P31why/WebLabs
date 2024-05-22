938be227790688b27eab81271c36287c664620e7
P31Dead <P31Dead@LAPETOPE>
2024-05-22 10:34:21 +0300

    pr12 update

--- пр 12/script.js
+++ пр 12/script.js
@@ -36,5 +36,102 @@ function t3(){
 	}
 }
 function t4(){
-	
+	var day=parseInt(document.form4.a.value);
+	switch (day) {
+                case 1:
+                    document.form4.out.value = "воскресенье";
+                    break;
+                case 2:
+                    document.form4.out.value = "понедельник";
+                    break;
+                case 3:
+                    document.form4.out.value = "вторник";
+                    break;
+                case 4:
+                    document.form4.out.value = "среда";
+                    break;
+                case 5:
+                    document.form4.out.value = "четверг";
+                    break;
+                case 6:
+                    document.form4.out.value = "пятница";
+                    break;
+                case 7:
+                    document.form4.out.value = "суббота";
+                    break;
+                case 8:
+                    document.form4.out.value = "воскресенье";
+                    break;
+                case 9:
+                    document.form4.out.value = "понедельник";
+                    break;
+                case 10:
+                    document.form4.out.value = "вторник";
+                    break;
+                case 11:
+                    document.form4.out.value = "среда";
+                    break;
+                case 12:
+                    document.form4.out.value = "четверг";
+                    break;
+                case 13:
+                    document.form4.out.value = "пятница";
+                    break;
+                case 14:
+                    document.form4.out.value = "суббота";
+                    break;
+                case 15:
+                    document.form4.out.value = "воскресенье";
+                    break;
+                case 16:
+                    document.form4.out.value = "понедельник";
+                    break;
+                case 17:
+                    document.form4.out.value = "вторник";
+                    break;
+                case 18:
+                    document.form4.out.value = "среда";
+                    break;
+                case 19:
+                    document.form4.out.value = "четверг";
+                    break;
+                case 20:
+                    document.form4.out.value = "пятница";
+                    break;
+                case 21:
+                    document.form4.out.value = "суббота";
+                    break;
+                case 22:
+                    document.form4.out.value = "воскресенье";
+                    break;
+                case 23:
+                    document.form4.out.value = "понедельник";
+                    break;
+                case 24:
+                    document.form4.out.value = "вторник";
+                    break;
+                case 25:
+                    document.form4.out.value = "среда";
+                    break;
+                case 26:
+                    document.form4.out.value = "четверг";
+                    break;
+                case 27:
+                    document.form4.out.value = "пятница";
+                    break;
+                case 28:
+                    document.form4.out.value = "суббота";
+                    break;
+                case 29:
+                    document.form4.out.value = "воскресенье";
+                    break;
+                case 30:
+                    document.form4.out.value = "понедельник";
+                    break;
+                case 31:
+                    document.form4.out.value = "вторник";
+                    break;
+                default:
+                    alert("В январе 2006 года такой даты не существует");
+            }
 }
