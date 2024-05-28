0feaab9e3b67c51c1615f874ce40ed9741377d5f
MindeRe <MindeRe@DESKTOP-CND4IL3>
2024-05-28 22:35:05 +0300

    pr14 update

--- пр 14/script.js
+++ пр 14/script.js
@@ -9,11 +9,24 @@ function t1(){
         table.innerHTML += "<tr><td>" + x + "</td><td>" + y + "</td></tr>";
 	}
 }
-function t3(){
+function t2(){
 	var table=document.getElementById("tableV2");
 	table.innerHTML="<tr><th> x </th></tr>";
 	var a=parseFloat(document.form2.inputA.value);
 	var b=parseFloat(document.form2.inputB.value);
+	var c=parseFloat(document.form2.inputC.value);
+	var i=1;
+	while(a<c){
+		a=a+a*0.1;
+		i++;
+	}
+	alert("за "+i+"дней");
+}
+function t3(){
+	var table=document.getElementById("tableV3");
+	table.innerHTML="<tr><th> x </th></tr>";
+	var a=parseFloat(document.form3.inputA.value);
+	var b=parseFloat(document.form3.inputB.value);
 	for(var x=a;x<=b;x++){
 		if(x%2!==0){
 			table.innerHTML += "<tr><td>" + x + "</td></tr>";		
