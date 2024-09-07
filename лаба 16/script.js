0ad7112d69e51a04a5695089c97c5e1e55e627fe
P31Dead <P31Dead@LAPETOPE>
2024-09-07 13:53:31 +0300

    updated laba16

--- лаба 16/script.js
+++ лаба 16/script.js
@@ -1,3 +1,10 @@
-function color_change(){
-	alert();
+var a=1;
+function color_change() {
+	if(a===1){
+		document.getElementsByClassName('text')[0].innerHTML ="red";
+	}
+}
+function fun1(){
+	var b=document.getElementById('input_num').value;
+	alert(Math.Pow(b,2));
 }
