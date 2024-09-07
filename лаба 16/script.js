5dcd09f881932395c3e964a407cbb656a4a3092b
P31Dead <P31Dead@LAPETOPE>
2024-09-07 15:56:25 +0300

    complete tasks in laba 16

--- лаба 16/script.js
+++ лаба 16/script.js
@@ -15,3 +15,10 @@ function fun1(){
 	var sq=document.getElementById('input_num').value;
 	alert('Результат вычислений: '+Math.pow(sq,2));
 }
+function fun2(){
+	var input1=document.getElementById('in1');
+	var input2=document.getElementById('in2');
+	var buff=input1.value;
+	input1.value=input2.value;
+	input2.value=buff;
+}
