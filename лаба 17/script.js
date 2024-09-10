a47b015dc27226dd673e61d1063f5ad9a8a5dd05
P31Dead <P31Dead@LAPETOPE>
2024-09-10 18:16:57 +0300

    updated laba 17

--- лаба 17/script.js
+++ лаба 17/script.js
@@ -1,3 +1,4 @@
+win=window.open("doc.html","wind","width=250,height=100,status=no,toolbar=no,menubar=no,scrollbars=yes");
 function summa(){
     var n1=parseInt(document.Sum3.number1.value)
     var n2=parseInt(document.Sum3.number2.value);
