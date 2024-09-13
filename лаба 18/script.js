8093ab3cb504eddd787efeacec6da6efc41b88ac
P31Dead <P31Dead@LAPETOPE>
2024-09-13 12:12:08 +0300

    updated laba 18

---
+++ лаба 18/script.js
@@ -0,0 +1,33 @@
+var images=['header_ctt.png','icon(512).png','images.jpg','unnamed.png'];
+var count=0;
+var folder='pictures/';
+function loadFirst(){
+    const im=document.getElementById('image');
+    im.src=folder+'header_ctt.png';
+}
+function nextImage(){
+    count++;
+    if(count>=images.length){
+        count=0;
+        const im=document.getElementById('image');
+        im.src=folder+images[count];
+    }
+    else {
+        const im=document.getElementById('image');
+        im.src=folder+images[count];
+    }
+    
+    
+}
+function previosImage(){
+    count--;
+    if(count<0){
+        count=images.length-1;
+        const im=document.getElementById('image');
+        im.src=folder+images[count];
+    }
+    else {
+        const im=document.getElementById('image');
+        im.src=folder+images[count];
+    }
+}
