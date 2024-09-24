607e0278c9b30ecdc869cfc20d20afc9b6d5d0b6
P31Dead <P31Dead@LAPETOPE>
2024-09-24 17:08:54 +0300

    laba 18 complete

--- лаба 18/script.js
+++ лаба 18/script.js
@@ -1,6 +1,46 @@
 var images=['header_ctt.png','icon(512).png','images.jpg','unnamed.png','1.png','Obito_Uchiha.png'];
 var count=0;
 var folder='pictures/';
+
+var imLibr=new Array();
+imLibr["image1"]=new Image(120,90);
+imLibr["image1"].src="pictures/1.png";
+imLibr["image2"]=new Image(120,90);
+imLibr["image2"].src="pictures/header_ctt.png";
+imLibr["image3"]=new Image(120,90);
+imLibr["image3"].src="pictures/icon(512).png";
+imLibr["image4"]=new Image(120,90);
+imLibr["image4"].src="pictures/images.jpg";
+
+function imgOver(event){
+    var im=event.target.id;
+    document.thum.src=imLibr[im].src;
+}
+
+function loadIm(list){
+    var b=(list.load.options.selectedIndex)
+    var img=list.load.options[b].value;
+    document.thum.src=imLibr[img].src;
+}
+
+var timeStop=false;
+var time = 5; // Задаём начальное время
+const timer = setInterval(() => {
+  document.getElementById('countdown').textContent = time;  // Останавливаем таймер, поскольку время истекло
+  if(time <= 0 && timeStop===true){
+    nextImage();
+    time=5;
+  }
+  else{
+    time--; 
+  }
+}, 1000); 
+
+function TimeButton(){
+    timeStop=!timeStop;
+}
+
+/*-----------------*/
 function loadFirst(){
     const im=document.getElementById('image');
     im.src=folder+'header_ctt.png';
